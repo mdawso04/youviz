f2b17f828f45ee80352542d7bff2897b4034329d
md <>
2022-08-22 06:48:32 +0900

    gui 5s

--- projects/components/vizreport.py
+++ projects/components/vizreport.py
@@ -1,6 +1,6 @@
 # django/unicorn/project
-from django_unicorn.components import UnicornView
-from projects.models import File, Item, Report
+from django_unicorn.components import QuerySetType, UnicornView
+from projects.models import Viz
 from project import settings
 from django.shortcuts import render, redirect
 
@@ -10,18 +10,54 @@ from pp.log import logger
 
 #python standard libraries
 import os
+from copy import *
 
 #non-standard libraries
 import pandas as pd
+import plotly.io as pio
 
 class VizreportView(UnicornView):
+    
+    vizs: QuerySetType[Viz] = None
+    vizs_html: list = []
     
 #LOAD/UPDATE
     
     def mount(self):
-        #logger.debug('DataframeView > mount start')
-        pass
-        #logger.debug('DataframeView > mount end')
+        #logger.debug('VizView > mount start')
+        self.load_vizs()
+        #logger.debug('VizView > mount end')
+    
+    def load_vizs(self):
+        #logger.debug('VizView > load_viz start')
+        
+        for v in self.vizs:
+            #load csv from db
+            copied_json = deepcopy(v.json)
+            copied_json[0]['options']['src'] = self.parent.file.databuffer
+            a = pp.App(copied_json)
+            fig = a.call(return_df=False)[0]
+            fig.update_layout(
+                height=370,
+                width=300,
+                autosize=True, 
+                margin={
+                    'l': 0,
+                    't': 15,
+                    'b': 68,
+                    'r': 10,
+                },
+                showlegend=True,
+                legend = {
+                    'x': 1,
+                    'xanchor': 'right',
+                    'y': 1,
+                    'bgcolor': '#000000',
+                }
+            )
+            self.vizs_html.append(pio.to_html(fig=fig))
+
+        #logger.debug('VizView > load_viz end')
     
     def hydrate(self):
         #logger.debug('DataframeView > hydrate start')
