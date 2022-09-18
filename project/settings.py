8c2dcc87413c19c9f7fbbda52ed9548b414c3d8b
md <>
2022-09-19 05:52:43 +0900

    test

--- project/settings.py
+++ project/settings.py
@@ -44,8 +44,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
                             'django-insecure-kfz#ag8=&yb1lynyfj0v=k%et%5++u08(q%@q(839gwqd-reah')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
