cdda7b44a5cbdf92295e15ade390aca58d0ae02b
md <>
2023-11-07 13:21:47 +1000

    5s

--- project/settings.py
+++ project/settings.py
@@ -85,6 +85,7 @@ else:
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
