0c910d64af996ab357e16c4efc942061b17e863d
md <>
2024-04-13 16:35:06 +1000

    5s

--- project/settings.py
+++ project/settings.py
@@ -48,6 +48,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
+#DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
