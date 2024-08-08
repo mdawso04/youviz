57c3b5a5da1f9a5c10c88f957886d13d023e58f4
md <>
2024-08-08 20:13:08 +1000

    map

--- project/sitemaps.py
+++ project/sitemaps.py
@@ -1,18 +1,26 @@
 # django_project/sitemaps.py
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from projects.models import Datasource
+from projects.models import Datasource, Profile
 
 class DatasourceSitemap(Sitemap):
     
     #changefreq = "never"
     priority = 0.5
+    list_urls = ['list',]
     
     def items(self):
-        return Datasource.objects.all()
+        return self.list_urls + list(Datasource.objects.all()) + list(Profile.objects.exclude(name__in=['System', 'Admin',]).all())
 
     def lastmod(self, obj):
+        if obj in self.list_urls:
+            return
         return obj.last_updated
     
     def location(self, obj):
-        return reverse('view', kwargs={'slug': obj.slug})
+        if obj in self.list_urls:
+            return reverse(obj)
+        elif isinstance(obj, Datasource):
+            return reverse('view', kwargs={'slug': obj.slug})
+        elif isinstance(obj, Profile):
+            return reverse('user', kwargs={'slug': obj.slug})
